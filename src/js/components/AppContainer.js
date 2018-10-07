07202df62f638e84accc39239d75b5e64b5a1aed
Disquietude <danny93ngo@gmail.com>
2018-10-07 04:11:53 -0400

    Added Modal Component

--- src/js/components/AppContainer.js
+++ src/js/components/AppContainer.js
@@ -2,6 +2,7 @@ import React from "react";
 import DEFAULT_RECIPES from "./defaultRecipes";
 import RecipeIndex from "./RecipeIndex/RecipeIndex";
 import RecipeView from "./RecipeView/RecipeView";
+import Modal from "./Modal/Modal";
 
 export default class AppContainer extends React.Component {
   constructor(props) {
@@ -15,6 +16,7 @@ export default class AppContainer extends React.Component {
       <div className="container">
         <RecipeIndex />
         <RecipeView />
+        <Modal />
       </div>
     );
   }
